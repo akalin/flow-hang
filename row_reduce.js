7bae056e0d1c7dd5a5e31fb6efa3bd6836a86789
Frederick Akalin <akalin@gmail.com>
2018-06-26 22:05:29 -0700

    Add second repro case

--- row_reduce.js
+++ row_reduce.js
@@ -13,82 +13,41 @@ import { Matrix } from './matrix';
 
 type RowReduceSwapState<T: Field<*>> = {
   type: 'swap',
-  aLeftPrev: Matrix<T>,
-  aRightPrev: Matrix<T>,
-  aLeft: Matrix<T>,
-  aRight: Matrix<T>,
-  rowA: number,
-  rowB: number,
+  m: Matrix<T>,
 };
 
 type RowReduceSingularState<T: Field<*>> = {
   type: 'singular',
-  aLeft: Matrix<T>,
-  aRight: Matrix<T>,
+  m: Matrix<T>,
 };
 
 type RowReduceDivideState<T: Field<*>> = {
   type: 'divide',
-  aLeftPrev: Matrix<T>,
-  aRightPrev: Matrix<T>,
-  aLeft: Matrix<T>,
-  aRight: Matrix<T>,
-  row: number,
-  divisor: Field<T>,
+  m: Matrix<T>,
 };
 
 type RowReduceSubtractScaledState<T: Field<*>> = {
   type: 'subtractScaled',
-  aLeftPrev: Matrix<T>,
-  aRightPrev: Matrix<T>,
-  aLeft: Matrix<T>,
-  aRight: Matrix<T>,
-  rowDest: number,
-  rowSrc: number,
-  scale: ?Field<T>,
-};
-
-type RowReduceInverseFoundState<T: Field<*>> = {
-  type: 'inverseFound',
-  aLeft: Matrix<T>,
-  aRight: Matrix<T>,
+  m: Matrix<T>,
 };
 
 type RowReduceState<T: Field<*>> =
   | RowReduceSwapState<T>
   | RowReduceSingularState<T>
   | RowReduceDivideState<T>
-  | RowReduceSubtractScaledState<T>
-  | RowReduceInverseFoundState<T>;
+  | RowReduceSubtractScaledState<T>;
 */
 
 // eslint-disable-next-line no-unused-vars
 const rowReduceNextState = /* :: <T: Field<*>> */ (
   state /* : RowReduceState<T> */
-) /* : RowReduceState<T> */ => {
-  const { aLeft, aRight } = state;
+) => {
+  const { m } = state;
 
-  const zero = aLeft.zeroElement();
-  for (let i = 0; i < aLeft.rows(); i += 1) {
-    const pivot = aLeft.at(i, i);
-    if (pivot.equals(zero)) {
-      return {
-        type: 'swap',
-        aLeftPrev: aLeft,
-        aRightPrev: aRight,
-        aLeft: aLeft.swapRows(i, i),
-        aRight: aRight.swapRows(i, i),
-        rowA: i,
-        rowB: i,
-      };
-    }
+  const zero = m.zeroElement();
+  if (zero.equals(zero)) {
+    zero.equals(zero);
   }
-
-  return {
-    type: 'inverseFound',
-    aLeft,
-    aRight,
-  };
 };
 
 /* ::
